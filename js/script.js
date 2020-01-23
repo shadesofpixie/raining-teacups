f814275f86ff66d0321baeb52a0b1d5900cae333
smilesandtea <smilesandcode@gmail.com>
2020-01-22 19:23:47 -0500

    Sending to git

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+
+var c = document.getElementById('canvas')
+var s = sprinkler.create(c)
+
+function rainingTea(message) {
+    var images = [
+        'teacup.png',
+        'teacup.png'
+      ]
+      var stop = s.start(images, {
+        ddyMin: 200,
+        ddyMax: 200
+      })
+}
+
+
+
+
+ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
+    if( command === "teaparty" &&
+        ( !rewardId || flags.broadcaster || flags.mod ) ) {
+        rainingTea( message );
+    }
+    if( flags.customReward && extra.customRewardId === rewardId ) {
+        rainingTea( message );
+    }
+};
+ComfyJS.onChat = ( user, message, flags, self, extra ) => {
+    if( flags.customReward && extra.customRewardId === "f1d2db18-77eb-4f0c-a0d7-5790cc735f01" ) {
+        rainingTea( message );
+    }
+};
+ComfyJS.Init( "smilesandteaa" );
